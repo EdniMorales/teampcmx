90a678b07771b4e395d594e6a1003e458e352fb5
EdniMorales <rimora.29@gmail.com>
2026-01-12 22:02:47 -0600

    Mejora la estructura del código y la configuración de Google Analytics en header.php

--- views/layout/header.php
+++ views/layout/header.php
@@ -2,44 +2,55 @@
 <html lang="en">
 
 <head>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-MW36T3RES7"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-MW36T3RES7"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'G-MW36T3RES7');
-</script>
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-MW36T3RES7');
+  </script>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <title>Team Pc Mx Servicios Informaticos expertos en TI | Creacion de paginas web </title>
   <meta name="description" content="pagina web,aplicaciones web,tu pagina web">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
   <link rel="shortcut icon" href="<?= base_url ?>/assets/img/logo/logo-team.png" />
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-9GLNT44VCT"></script>
 
-
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link href="<?= base_url ?>assets/css/animated.css" rel="stylesheet" />
   <link href="<?= base_url ?>assets/css/infinite-slider.css" rel="stylesheet" />
   <link href="<?= base_url ?>assets/css/modern-business.css" rel="stylesheet" />
   <link href="<?= base_url ?>assets/css/scrolling-nav.css" rel="stylesheet" />
   <link href="<?= base_url ?>assets/css/estilos.css" rel="stylesheet" />
- <!--  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <!--  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+  </script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
- 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script><!-- desfgloce de menu -->
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- desfgloce de menu -->
   <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
   <script src="https://unpkg.com/gijgo@1.9.13/js/messages/messages.es-es.js" type="text/javascript"></script>
   <script src="https://kit.fontawesome.com/1849e1867b.js" crossorigin="anonymous"></script>
@@ -51,29 +62,30 @@
   <script src="<?= base_url ?>assets/js/slick.min.js"></script>
 </head>
 
-
 <body class="sb-nav-fixed">
-  <div id="container" >
+  <div id="container">
 
-  <nav class="text-white navbar navbar-expand-lg fixed-top" style="background-color: #000000ff;">
-    <div class="container">
-  <a class="navbar-brand" href="<?= base_url ?>">
-              <img src="<?= base_url ?>assets/img/logo/logo-team.png" class="d-inline-block align-top" width="100%" height="60" alt="">
-         
-            </a>
-  <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <nav class="text-white navbar navbar-expand-lg fixed-top" style="background-color: #000000ff;">
+      <div class="container">
+        <a class="navbar-brand" href="<?= base_url ?>">
+          <img src="<?= base_url ?>assets/img/logo/logo-team.png" class="d-inline-block align-top" width="100%"
+            height="60" alt="">
+        </a>
+        <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse"
+          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-<!--       <li class="nav-item active">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <!--       <li class="nav-item active">
         <a class="menu-list" href="#"> <span class="sr-only">(current)</span></a>
       </li> -->
-   <!--    <li class="nav-item">
+            <!--    <li class="nav-item">
         <a class="menu-list" href="#">Camaras de Vigilancia</a>
       </li> -->
-<!--       <li class="nav-item dropdown">
+            <!--       <li class="nav-item dropdown">
         <a class="menu-list dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Punto de Venta
         </a>
@@ -87,17 +99,18 @@
        <a class="dropdown-item" href="#">Something else here</a> 
         </div>
       </li> -->
-    <!--   <li class="nav-item">
+            <!--   <li class="nav-item">
               <a class="menu-list" href="<?= base_url ?>Soporte/index" style="">SOPORTE TECNICO</a>
             </li> -->
-      <li class="nav-item">
-        <a class="menu-list" href="<?= base_url ?>desarrollo/index">PAGINAS WEB</a>
-      </li>
-          <li class="nav-item">
-              <a class="btn btn-success" href="tel:+525518055852" role="button"><i class="fa fa-whatsapp" aria-hidden="true"></i> 55 1805 5852</a>
+         <!--    <li class="nav-item">
+              <a class="menu-list" href="<?= base_url ?>desarrollo/index">PAGINAS WEB</a>
+            </li> -->
+            <li class="nav-item">
+              <a class="btn btn-success" href="tel:+525518055852" role="button"><i class="fa fa-whatsapp"
+                  aria-hidden="true"></i> 55 1805 5852</a>
             </li>
-      
-     <!--  <li class="nav-item dropdown">
+
+            <!--  <li class="nav-item dropdown">
         <a class="menu-list dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
          Community Manager
         </a>
@@ -108,21 +121,21 @@
          <a class="dropdown-item" href="#">Something else here</a> 
         </div>
       </li> -->
-     <!--  <li class="nav-item">
+            <!--  <li class="nav-item">
         <a class="nav-link disabled" href="#">Disabled</a>
       </li> -->
-    </ul>
-   <!--  <form class="form-inline my-2 my-lg-0">
+          </ul>
+          <!--  <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form> -->
-  </div>
+        </div>
 
-  </div>
-</nav>
+      </div>
+    </nav>
 
-  <div>
-<!--   
+    <div>
+      <!--   
   <nav class="navbar navbar-expand-lg navbar-light fixed-top text-white" id="" style="background-color: #000000;">
       <div class="container" id="cont-cero">
         <div id="cont-fig" class="">
