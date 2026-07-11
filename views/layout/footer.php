ee78e8434db20e6bc26b38a3698c50751de7f905
EdniMorales <rimora.29@gmail.com>
2026-07-11 13:49:20 -0600

    Añade nuevas clases de texto con fuente Agbalumo en el pie de página y actualiza la presentación de los títulos.

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,6 +1,5 @@
 </div>
 </main>
-<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.3805587889183!2d-98.89948508468514!3d19.265809950984174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce1f3231d3db91%3A0x75bd80c362b53ae5!2sTICDWEM!5e0!3m2!1ses-419!2smx!4v1629138357273!5m2!1ses-419!2smx" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> -->
 <footer class="page-footer font-small unique-color-dark" style="background-color: #000000ff;">
   <div style="background-color: #00a8ff;">
     <div class="container">
@@ -21,19 +20,19 @@
   <div class="container text-center text-md-left mt-5">
     <div class="row mt-3">
       <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-        <h6 class="text-uppercase font-weight-bold text-light text-center">Team Pc Mx</h6>
+        <p class="text-uppercase font-weight-bold text-light text-center txt-foot-2">Team Pc Mx</p>
         <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 0px;">
         <p class="text-light">Ofreciendo una solución global a Empresas,Pymes,Administraciones y Usuarios particulares.</p>
        <!--  <p class=""><img src="<?= base_url ?>assets/img/logo-elektra.png" class="text-center" width="100%" height="75px" alt=""></p> -->
       </div>
       <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-        <h6 class="text-uppercase font-weight-bold text-light">EN Colaboracion con</h6>
+        <p class="text-uppercase font-weight-bold text-light txt-foot-3"> Colaboradores</p>
         <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 0px;">
         <p class="text-center"><img src="<?= base_url ?>assets/img/images/movilpacklogo.png" class="text-center" width="90px" height="60px" alt=""></p>
         <p class="text-uppercase font-weight-bold text-light text-center">¡ACEPTAMOS PAGO CON TARJETAS!</p>
       </div>
       <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-        <h6 class="text-uppercase font-weight-bold text-light">Menu</h6>
+        <p class="text-uppercase font-weight-bold text-light txt-foot-3">Menu</p>
         <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 0px;">
        
         <p><a href="<?= base_url ?>Soporte/index" class="text-white">Soporte Tecnico</a></p>
@@ -41,7 +40,7 @@
         <p><a href="<?= base_url ?>#contact" class="text-white">Envia un mensaje</a></p>
       </div>
       <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-        <h6 class="text-uppercase font-weight-bold text-light">Contacto</h6>
+        <p class="text-uppercase font-weight-bold text-light txt-foot-3">Contacto</p>
         <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto " style="width: 00px;">
         <p class="text-light"><i class="fa fa-map-marker mr-2"></i><a href="https://goo.gl/maps/D6494QNjkHq7u3MZ8" class="text-white">Geo Pueblo Nuevo Chalco C.P 56600 Chalco de Díaz Covarrubias Centro, Estado de Mexico</a></p>
         <p class="text-light"><i class="fa fa-envelope mr-2"></i><a href="mailto:info@teampcmx.com" class="text-white">info@teampcmx.com</a></p>
