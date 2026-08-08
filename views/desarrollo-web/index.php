29d6ef3f523e107dfea08f7d30ea71bff8d201f2
EdniMorales <rimora.29@gmail.com>
2026-08-08 14:02:51 -0600

    Añade clase sect-dev-1 para ajustar márgenes en el encabezado de la sección de desarrollo web.

--- views/desarrollo-web/index.php
+++ views/desarrollo-web/index.php
@@ -1,5 +1,5 @@
 
-            <header class="bg-dark py-5">
+            <header class="sect-dev-1 bg-dark py-5">
                 <div class="container px-5">
                     <div class="row gx-5 align-items-center justify-content-center">
                         <div class="col-lg-8 col-xl-7 col-xxl-6">
